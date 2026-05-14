e028a8233034a1edd5303edab1d53a1b5766f3d6
Hidayah Admin <hidayah-admin@placeholder.com>
2026-05-14 12:23:37 +0100

    Add missing transaction import in payments/views.py

--- backend/payments/views.py
+++ backend/payments/views.py
@@ -11,6 +11,7 @@ from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
 from django.utils import timezone
 from django.conf import settings
+from django.db import transaction
 from decimal import Decimal
 import logging
 
